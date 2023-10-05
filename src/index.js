f13b1929d3bea191a675ac31dbc35608f4fdef05
igorocky <123@gmail.com>
2023-10-05 10:44:48 +0200

    Move frontend files to "fe" directory.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './fe/resources/index.css';
+import App from './fe/js/App';
+import reportWebVitals from './fe/js/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
